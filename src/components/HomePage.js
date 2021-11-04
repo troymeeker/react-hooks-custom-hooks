d818713b05940272567b6698569c3d289215ed3d
troymeeker <troymeeker1@gmail.com>
2021-11-04 15:11:36 -0700

    complete

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,26 +1,27 @@
 import React, { useEffect, useState } from "react";
+import useQuery from "../hooks/useQuery";
 import About from "./About";
 import ArticleList from "./ArticleList";
+import useDocumentTitle from "../hooks/useDocumentTitle";
 
 function HomePage() {
   // fetch data for posts
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [posts, setPosts] = useState([]);
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // const [posts, setPosts] = useState([]);
+  const {data: posts, isLoaded} = useQuery("http://localhost:400/posts");
 
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch("http://localhost:4000/posts")
-      .then((r) => r.json())
-      .then((posts) => {
-        setPosts(posts);
-        setIsLoaded(true);
-      });
-  }, []);
+  // useEffect(() => {
+  //   setIsLoaded(false);
+  //   fetch("http://localhost:4000/posts")
+  //     .then((r) => r.json())
+  //     .then((posts) => {
+  //       setPosts(posts);
+  //       setIsLoaded(true);
+  //     });
+  // }, []);
 
   // set the document title
-  useEffect(() => {
-    document.title = "Underreacted | Home";
-  }, []);
+ useDocumentTitle("Underreacted | Home");
 
   return (
     <>
